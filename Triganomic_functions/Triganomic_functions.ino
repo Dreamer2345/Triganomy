d4778f31b678d7ca64973868a8181d573db4e497
Dreamer2345 <33360259+Dreamer2345@users.noreply.github.com>
2018-01-14 01:26:14 +0000

    Add files via upload

---
+++ Triganomic_functions/Triganomic_functions.ino
@@ -0,0 +1,24 @@
+#include <FixedPoints.h>
+#include <FixedPointsCommon.h>
+#include <Arduboy2.h>
+Arduboy2 ard;
+
+#include "Trig.h"
+
+
+uint8_t dex = 0;
+void setup() {
+  ard.begin();
+}
+
+void loop() {
+  ard.clear();
+  
+  for(uint8_t i=0; i<128; ++i)
+    {
+    ard.drawPixel(i,round(static_cast<float>(Sin(i+dex))*10)+31);
+    ard.drawPixel(i,round(static_cast<float>(Cos(i+dex))*10)+31);
+    }
+  dex++;
+  ard.display();
+}
